4290f86b062ca6d41a907a87c4320afafe68283f
João Vitor <joaozanini.code@gmail.com>
2023-06-01 08:56:04 -0300

    .

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.send("<h1>Olá, Mundo!</h1>")
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res) {
+    var n1 = Number(req.body.n1);
+    var n2 = Number(req.body.n2);
+    
+    res.send(`${n1 + n2}`)
 })
 
 app.listen(8081, function() {
